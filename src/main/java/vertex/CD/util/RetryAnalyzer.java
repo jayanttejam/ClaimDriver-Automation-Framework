5bff92f707a68807869597fa51318aa78a7f9a8f
jayanttejam <jayanttejam@gmail.com>
2026-04-17 16:12:51 +0530

    added properties file to test/resources

--- src/main/java/vertex/CD/util/RetryAnalyzer.java
+++ src/main/java/vertex/CD/util/RetryAnalyzer.java
@@ -6,7 +6,7 @@ import org.testng.ITestResult;
 public class RetryAnalyzer implements IRetryAnalyzer {
 
     private int count = 0;
-    private static final int MAX_RETRY = 2;
+    private static final int MAX_RETRY = 1;
 
     @Override
     public boolean retry(ITestResult result) {
